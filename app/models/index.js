a3b77bb8b64afae989d7926a065f4db884e626a6
Thakun098 <thakun.tant@northbkk.ac.th>
2025-05-07 16:22:23 +0700

    updated

---
+++ app/models/index.js
@@ -0,0 +1,38 @@
+const config = require("../config/db.config");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        port: config.PORT,
+        dialect: config.DIALECT,
+        pool: {
+            max: config.pool.max,
+            min: config.pool.min,
+            acquire: config.pool.acquire,
+            idle: config.pool.idle
+        }
+    }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+db.user = require("../models/ีuser.model")(sequelize, Sequelize);
+db.role = require("../models/role.model")(sequelize, Sequelize);
+
+
+//Relationship (Many to Many)
+db.role.belongsToMany(db.user, {
+    through: "user_roles"
+});
+db.user.belongsToMany(db.role, {
+    through: "user_roles"
+});
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv/config");
+const app = express();
+const db = require("./app/models");
+const cors = require("cors");
+
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+db.sequelize.sync({ force: false })
+    .then(() => {
+        console.log("Database Sync...")
+    })
+
+app.get('/', (req, res) => {
+    res.send('Hello Elyia');
+    // console.log("Hello Elysia");
+});
+
+
+
+require("./app/routes/auth.routes")(app);
+
+const port = process.env.SERVER_PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} `)
+});
